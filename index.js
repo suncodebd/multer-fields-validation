803ad00160a8f271c70a8da93bfebe55d1464184
Md Minarul islam <101469612+suncodebd@users.noreply.github.com>
2022-06-24 22:49:41 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,28 @@
 /* eslint-disable prettier/prettier */
 const express = require('express');
 const multer = require('multer');
+const path = require('path');
 
-// Upload foldr
+// File Upload foldr
 const UPLOADS_FOLDER = './upload';
+
+// define the storage
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, UPLOADS_FOLDER);
+    },
+    filename: (req, file, cb) => {
+        // impotant file.pdf
+        const fileExt = path.extname(file.originalname);
+        const fileName = `${file.originalname.replace(fileExt, '').toLowerCase().split(' ').join('-')}-${Date.now()}`;
+
+        cb(null, fileName + fileExt);
+    },
+});
 
 // package the final multer uppload object
 const upload = multer({
-    dest: UPLOADS_FOLDER,
+    storage,
     limits: {
         fileSize: 1000000, // 1MB
     },
